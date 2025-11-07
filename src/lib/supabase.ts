d707d2fb08d0c86b00283e041d125a79b91b7530
HenricoAmalfi03 <kernelpanic10190@gmail.com>
2025-11-07 14:15:19 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zdteeaiznhxmbmftiunw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkdGVlYWl6bmh4bWJtZnRpdW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NzY0MjIsImV4cCI6MjA3ODA1MjQyMn0.9GKjRn2sslWFsHARE_XuRnFfC9UApe-WpwrBQ9kTdd4';
+const supabaseUrl = 'https://drbqmjmsscqovtsqmszf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyYnFtam1zc2Nxb3Z0c3Ftc3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjcyNDUsImV4cCI6MjA3ODEwMzI0NX0.RLfT9b-5y2Q_yQD1oeq0y73seNZxz1WRFqJe8zm1n_A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
