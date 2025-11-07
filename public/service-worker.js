dd0807ca4a5d43990a9fc431b391e094d702289f
HenricoAmalfi03 <kernelpanic10190@gmail.com>
2025-11-07 17:38:52 -0300

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: ativo');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Mantém comportamento padrão (sem cache ainda)
+});
