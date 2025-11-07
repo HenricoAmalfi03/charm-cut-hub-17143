a04228072a894b54bc353f4a5c16502391284caa
HenricoAmalfi03 <kernelpanic10190@gmail.com>
2025-11-07 13:37:11 -0300

    Update Supabase URL in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zdteeaiznhxmbmftiunw.supabase.co';
+const supabaseUrl = 'https://drbqmjmsscqovtsqmszf.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkdGVlYWl6bmh4bWJtZnRpdW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NzY0MjIsImV4cCI6MjA3ODA1MjQyMn0.9GKjRn2sslWFsHARE_XuRnFfC9UApe-WpwrBQ9kTdd4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
